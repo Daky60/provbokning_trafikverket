2eaf4d66f1ace6ab00e449fc9481ca31ab2d5e1b
Daky <mail@sebfrodin.se>
2020-07-17 00:19:15 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,3 @@
-# https://sites.google.com/a/chromium.org/chromedriver/home
-# Get same version as your chrome
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
