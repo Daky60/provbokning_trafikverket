5794669198999000af5fb7d077ddcee9d4abaab7
Daky <mail@sebfrodin.se>
2021-09-04 11:11:35 +0200

    Slagit ihop bla rent_option och trimmat ned lite

--- config_sample.py
+++ config_sample.py
@@ -1,17 +1,8 @@
 ## Se README.md
 
 social_security = 'yyyymmddxxxx'
-license_type = 'B'
-
-## For written exam
-#exam = 'Kunskapsprov'
-#language_option = 'Svenska'
-
-## For driving test
-exam = 'Körprov'
-rent_option = 'Ja, manuell'
-
-dates = ['2021-10-26', '2021-12-20']
-locations = ['Sollentuna', 'Järfälla', 'Farsta']
-
-wait_time = 2 ##
+license_type = 'A'
+exam = 'Kunskapsprov'
+rent_or_language = 'Ja, manuell'
+dates = ['2021-08-26', '2021-12-20']
+locations = ['Järfälla', 'Farsta']
