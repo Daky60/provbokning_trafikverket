e73857d1b8cbff85603a1c009c5401cbf74f6d74
Daky <mail@sebfrodin.se>
2021-09-04 11:10:15 +0200

    Skrivit om all kod för läsbarhet och lättare underhåll

--- bot.py
+++ bot.py
@@ -8,160 +8,89 @@ import time
 import config
 from playsound import playsound
 
-driver = webdriver.Chrome('chromedriver')
-driver.get('https://fp.trafikverket.se/boka/#/licence')
-driver.implicitly_wait(config.wait_time/10) ## seconds
-
-
-
-## Incorrect config handling
-try:
-    print('Read README.md and configure config.py before running script')
-    ### social security
-    if len(config.social_security) != 12:
-        print('social_security may be configured incorrectly')
-        print('social_security:', config.social_security)
-    ### license_type 
-    if not config.license_type[0].isupper():
-        print('license_type may be configured incorrectly')
-        print('license_type:', config.license_type)
-    if ('Körprov' not in config.exam) or ('Kunskapsprov' not in config.exam):
-        print('exam or license_type may be configured incorrectly')
-        print('exam:', config.exam)
-        print('license_type:', config.license_type)
-    if config.rent_option and config.language_option:
-        print('rent_option or rent_option may be configured incorrectly')
-        print('you should probably remove one')
-        print('rent_option:', config.rent_option)
-        print('language_option:', config.language_option)
-    if (len(config.dates) % 2) != 0 or not type(config.dates) == list:
-        print('dates may be configured incorrectly')
-        print('dates:', config.dates)
-    if not type(config.locations) == list:
-        print('locations may be configured incorrectly')
-        print('locations:', config.locations)
-except:
-    pass
-
-
-
-## Inputs SS and license type
-def step_one(social_security, license_type):
-    social_security_element = WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.ID, "social-security-number-input"))
-    )
-    social_security_element.send_keys(social_security)
-    driver.find_element_by_xpath(f'//*[@title="{license_type}"]').click()
-
-
-## Selects exam
-def step_two(exam):
-    try:
-        exam_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.XPATH, f"//*[text()='{exam}']"))
-        )
-        exam_element.click()
-        exam_element_parent = exam_element.find_element_by_xpath('..')
-        exam_element_parent.click()
-    except:
-        pass
-
-    
-
-
-## Selects rent or language option
-def step_three(option_type=None, option_value=None):
-    try:
-        if option_type == "rent":
-            rent_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f"//select[@id='vehicle-select']/option[text()='{option_value}']"))
+class SeleniumDriver():
+    def __init__(self):
+        self.driver = webdriver.Chrome('chromedriver')
+        self.driver.get('https://fp.trafikverket.se/boka/#/licence')
+        self.driver.implicitly_wait(0.2)
+        self.continue_running = True
+    def select_exam(self):
+        try:
+            while self.driver.title == "Förarprov - Bokning":
+                social_security_element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "social-security-number-input"))
+                )
+                social_security_element.send_keys(config.social_security)
+                self.driver.find_element_by_xpath(f"//*[@title='{config.license_type}']").click()
+        except:
+            pass
+    def select_exam_type(self):
+        try:
+            exam_element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, f"//*[text()='{(config.exam)}']"))
             )
-            rent_element.click()
-        if option_type == "language":
-            language_element = WebDriverWait(driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f"//select[@id='language-select']/option[text()='{option_value}']"))
+            exam_element.click()
+            exam_element_parent = exam_element.find_element_by_xpath('..')
+            exam_element_parent.click()
+        except:
+            pass
+    def select_rent_or_language(self):
+        try:
+            rent_or_language_element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, f"//select[@id='vehicle-select']/option[text()='{config.rent_or_language}']"))
             )
-            language_element.click()
-    except:
-        pass
+            rent_or_language_element.click()
+        except:
+            pass
+    def select_location(self, location):
+        self.location = location
+        try:
+            location_element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "id-control-searchText-1-1"))
+            )
+            location_element.clear()
+            location_element.send_keys(self.location, Keys.ENTER)
+        except:
+            pass
+    def select_time(self, first_date, last_date):
+        self.first_date = first_date
+        self.last_date = last_date
+        try:
+            WebDriverWait(self.driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, f"//*[text()='Lediga provtider']"))
+            )
+            self.first_date = datetime.strptime(self.first_date, '%Y-%m-%d').date()
+            self.last_date = datetime.strptime(self.last_date, '%Y-%m-%d').date()
+            while self.first_date < self.last_date:
+                try:
+                    find_date = self.driver.find_element_by_xpath(f"//*[contains(text(), '{str(self.first_date)}')]")
+                    if find_date:
+                        find_date_button = find_date.find_element_by_xpath(f"//*[text()='Välj']")
+                        find_date_button.click()
+                        return False
+                except:
+                    pass
+                finally:
+                    if self.first_date <= self.last_date:
+                        self.first_date = self.first_date + timedelta(days=1)
+        except:
+            pass
+        return True
 
-
-## Selects location
-def select_location(loc):
-    try:
-        location_element = WebDriverWait(driver, 10).until(
-            EC.presence_of_element_located((By.ID, "id-control-searchText-1-1"))
-        )
-        location_element.clear()
-        location_element.send_keys(loc, Keys.ENTER)
-    except:
-        pass
-
-## Finds and selects available time based on arguments
-def book_time(first_date, last_date):
-    try:
-        WebDriverWait(driver, 10).until(
-        EC.presence_of_element_located((By.XPATH, f"//*[text()='Lediga provtider']"))
-        )
-        first_date = datetime.strptime(first_date, '%Y-%m-%d').date()
-        last_date = datetime.strptime(last_date, '%Y-%m-%d').date()
-        while first_date < last_date:
-            try:
-                find_date = driver.find_element_by_xpath(f"//*[contains(text(), '{str(first_date)}')]")
-                if find_date:
-                    find_date_button = find_date.find_element_by_xpath(f"//*[text()='Välj']")
-                    find_date_button.click()
-                    return False
-            except:
-                pass
-            finally:
-                if first_date <= last_date:
-                    first_date = first_date + timedelta(days=1)
-    except:
-        pass
-    return True
-
-
-## continue regardless of strings existance
-
-
-## puts it together
-## initial pages etc
-step_one(config.social_security, config.license_type)
-step_two(config.exam)
-time.sleep(config.wait_time)
-time.sleep(config.wait_time)
-## final page
-
-def final_page():
-    step_three_conf = False;
-    try:
-        if config.rent_option:
-            step_three_conf_type = "rent"
-            step_three_conf_value = config.rent_option
-            step_three_conf = config.rent_option
-            step_three_conf = True
-        if config.language_option:
-            step_three_conf_type = "language"
-            step_three_conf_value = config.language_option
-            step_three_conf = True
-    except:
-        pass
-    continue_running = True
-    while continue_running:
+def find_exam(driver):
+    driver.select_exam()
+    while driver.continue_running:
         for i in config.locations:
             try:
-                step_two(config.exam)
-                if continue_running:
-                    if step_three_conf and step_three_conf_type and step_three_conf_value:
-                        step_three(step_three_conf_type, step_three_conf_value)
-                    select_location(i)
-                    time.sleep(config.wait_time/2)
-                    ## for handling multiple timespans
+                driver.select_exam_type()
+                driver.select_rent_or_language()
+                if driver.continue_running:
+                    driver.select_location(i)
+                    time.sleep(0.5)
                     for j in range(0, len(config.dates), 2):
-                        continue_running = book_time(config.dates[j], config.dates[j+1])
-                        time.sleep(config.wait_time/4)
-                        if not continue_running:
+                        driver.continue_running = driver.select_time(config.dates[j], config.dates[j+1])
+                        time.sleep(0.4)
+                        if not driver.continue_running:
                             timestamp = datetime.now() + timedelta(minutes=15)
                             while datetime.now() < timestamp:
                                 playsound('sounds/alert.mp3')
@@ -170,8 +99,5 @@ def final_page():
             except:
                 pass
 
-
 if __name__ == '__main__':
-    final_page()
-
-driver.quit()
+    find_exam(SeleniumDriver())
