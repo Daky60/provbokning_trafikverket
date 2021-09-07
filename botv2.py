53cb7afad30a5f7ed1f69ed7200ac77cfbb03e6e
Daky <mail@sebfrodin.se>
2021-09-07 18:41:30 +0200

    La till till felhantering för att lättare upptäcka uppdatering på sidan osv

--- botv2.py
+++ botv2.py
@@ -4,6 +4,8 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from datetime import datetime, timedelta
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 import time
 import config
 from playsound import playsound
@@ -20,24 +22,29 @@ class SeleniumDriver():
                 EC.presence_of_element_located((By.XPATH, f"//*[text()='Boka prov']"))
             )
             login_element.click()
-            login_element_parent = login_element.find_element_by_xpath('..')
-            login_element_parent.click()
-        except:
-            pass
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong")
     def enter_social_security(self):
         try:
             social_security_element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "social-security-number-input"))
                 )
+            time.sleep(5)
             social_security_element.send_keys(config.social_security)
             continue_element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[text()='Fortsätt']"))
             )
             continue_element.click()
-            continue_element_element_parent = continue_element.find_element_by_xpath('..')
-            continue_element_element_parent.click()
-        except:
-            pass
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong")
     def select_exam(self):
         try:
             exam_element = WebDriverWait(self.driver, 10).until(
@@ -46,16 +53,13 @@ class SeleniumDriver():
             exam_element.click()
             exam_element_parent = exam_element.find_element_by_xpath('..')
             exam_element_parent.click()
-        except:
-            pass
-    def perform_successful_login(self):
-        #better check if succesful needed
-        self.login()
-        time.sleep(5)
-        self.enter_social_security()
-        time.sleep(5)
-        self.select_exam()
-        time.sleep(5)
+            return True
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong. Check config or report on Github")
     def select_exam_type(self):
         try:
             exam_type_element = WebDriverWait(self.driver, 10).until(
@@ -64,6 +68,8 @@ class SeleniumDriver():
             exam_type_element.click()
             exam_element_parent = exam_type_element.find_element_by_xpath('..')
             exam_element_parent.click()
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
         except:
             pass
     def select_rent_or_language(self):
@@ -72,6 +78,8 @@ class SeleniumDriver():
                 EC.presence_of_element_located((By.XPATH, f"//select[@id='vehicle-select']/option[text()='{config.rent_or_language}']"))
             )
             rent_or_language_element.click()
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
         except:
             pass
     def select_location(self, location):
@@ -82,6 +90,8 @@ class SeleniumDriver():
             )
             location_element.clear()
             location_element.send_keys(self.location, Keys.ENTER)
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
         except:
             pass
     def select_time(self, first_date, last_date):
@@ -100,6 +110,8 @@ class SeleniumDriver():
                         find_date_button = find_date.find_element_by_xpath(f"//*[text()='Välj']")
                         find_date_button.click()
                         return False
+                except NoSuchElementException:
+                    print("Could not find element. Check config or report on Github")
                 except:
                     pass
                 finally:
@@ -116,8 +128,12 @@ class SeleniumDriver():
             exam_element.click()
             exam_element_parent = exam_element.find_element_by_xpath('..')
             exam_element_parent.click()
-        except:
-            pass
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong")
     def confirm_booking(self):
         try:
             exam_element = WebDriverWait(self.driver, 10).until(
@@ -126,8 +142,12 @@ class SeleniumDriver():
             exam_element.click()
             exam_element_parent = exam_element.find_element_by_xpath('..')
             exam_element_parent.click()
-        except:
-            pass
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong")
     def confirm_phone_number(self):
         try:
             phone_number_element = WebDriverWait(self.driver, 10).until(
@@ -137,8 +157,12 @@ class SeleniumDriver():
                 EC.presence_of_element_located((By.ID, "input-second-phone"))
                 )
             phone_number_element2.send_keys(phone_number_element)
-        except:
-            pass
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong")
     def confirm_last(self):
         try:
             exam_type_element = WebDriverWait(self.driver, 10).until(
@@ -147,12 +171,20 @@ class SeleniumDriver():
             exam_type_element.click()
             exam_element_parent = exam_type_element.find_element_by_xpath('..')
             exam_element_parent.click()
-        except:
-            pass
-
+        except TimeoutException:
+            print("Took too long to load")
+        except NoSuchElementException:
+            print("Could not find element. Check config or report on Github")
+        except Exception:
+            print("Something went wrong")
 
 def find_exam(driver):
-    driver.perform_successful_login()
+    driver.login()
+    time.sleep(5)
+    driver.enter_social_security()
+    time.sleep(5)
+    driver.select_exam()
+    time.sleep(5)
     while driver.continue_running:
         for i in config.locations:
             try:
