524f4546eda615d2d273fc635f173ce192809ef3
Daky <62486063+Daky60@users.noreply.github.com>
2022-01-28 16:45:16 +0100

    Update config_sample.py
    Uppdaterade i enlighet med trafikverkets ändring av sidotext iom fler fastnat pga ändringarna

--- config_sample.py
+++ config_sample.py
@@ -1,8 +1,8 @@
 ## Se README.md
 
 social_security = 'yyyymmddxxxx'
-license_type = 'A'
-exam = 'Kunskapsprov'
-rent_or_language = 'Ja, manuell'
+license_type = 'B'
+exam = 'Körprov'
+rent_or_language = 'Manuell bil'
 dates = ['2021-08-26', '2021-12-20']
 locations = ['Järfälla', 'Farsta']
