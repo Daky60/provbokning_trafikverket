322bbb183c72e72e85dd543ec5aab8e57542c3f2
Daky <62486063+Daky60@users.noreply.github.com>
2022-04-02 16:19:02 +0200

    Uppdaterade till senaste versionen av Selenium
    Uppdaterade find_element_by_xpath till find_element bland annat

--- botv2.py
+++ botv2.py
@@ -6,22 +6,25 @@ from datetime import datetime, timedelta
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_experimental_option("excludeSwitches", ["enable-logging"])
 import time
 import config
 from playsound import playsound
 
 class SeleniumDriver():
     def __init__(self):
-        self.driver = webdriver.Chrome('chromedriver')
+        self.driver = webdriver.Chrome('chromedriver', options=chrome_options)
         self.driver.get('https://fp.trafikverket.se/boka/#/')
         self.driver.implicitly_wait(0.1)
         self.continue_running = True
     def login(self):
         try:
-            login_element = WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[text()='Boka prov']"))
-            )
-            login_element.click()
+            ).click()
         except TimeoutException:
             print("Took too long to load")
         except NoSuchElementException:
@@ -35,10 +38,9 @@ class SeleniumDriver():
                 )
             time.sleep(5)
             social_security_element.send_keys(config.social_security)
-            continue_element = WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[text()='Fortsätt']"))
-            )
-            continue_element.click()
+            ).click()
         except TimeoutException:
             print("Took too long to load")
         except NoSuchElementException:
@@ -47,12 +49,9 @@ class SeleniumDriver():
             print("Something went wrong")
     def select_exam(self):
         try:
-            exam_element = WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[@title='{config.license_type}']"))
-            )
-            exam_element.click()
-            exam_element_parent = exam_element.find_element_by_xpath('..')
-            exam_element_parent.click()
+            ).click()
             return True
         except TimeoutException:
             print("Took too long to load")
@@ -65,19 +64,16 @@ class SeleniumDriver():
             exam_type_element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[text()='{(config.exam)}']"))
             )
-            exam_type_element.click()
-            exam_element_parent = exam_type_element.find_element_by_xpath('..')
-            exam_element_parent.click()
+            exam_type_element.find_element(By.XPATH, '..').click()
         except NoSuchElementException:
             print("Could not find element. Check config or report on Github")
         except:
             pass
     def select_rent_or_language(self):
         try:
-            rent_or_language_element = WebDriverWait(self.driver, 10).until(
+            WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//select[@id='vehicle-select']/option[text()='{config.rent_or_language}']"))
-            )
-            rent_or_language_element.click()
+            ).click()
         except NoSuchElementException:
             print("Could not find element. Check config or report on Github")
         except:
@@ -105,9 +101,9 @@ class SeleniumDriver():
             self.last_date = datetime.strptime(self.last_date, '%Y-%m-%d').date()
             while self.first_date < self.last_date:
                 try:
-                    find_date = self.driver.find_element_by_xpath(f"//*[contains(text(), '{str(self.first_date)}')]")
+                    find_date = self.driver.find_element(By.XPATH, f"//*[contains(text(), '{str(self.first_date)}')]")
                     if find_date:
-                        find_date_button = find_date.find_element_by_xpath(f"//*[text()='Välj']")
+                        find_date_button = find_date.find_element(By.XPATH,f"//*[text()='Välj']")
                         find_date_button.click()
                         return False
                 except:
@@ -119,13 +115,12 @@ class SeleniumDriver():
             pass
         return True
     def book_exam(self):
+        time.sleep(5)
         try:
             exam_element = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, f"//*[text()='Gå vidare']"))
+                EC.presence_of_element_located((By.XPATH, f"//button[@class='btn btn-lg btn-primary col-xs-12']"))
             )
-            exam_element.click()
-            exam_element_parent = exam_element.find_element_by_xpath('..')
-            exam_element_parent.click()
+            self.driver.execute_script("arguments[0].click();", exam_element)
         except TimeoutException:
             print("Took too long to load")
         except NoSuchElementException:
@@ -137,9 +132,7 @@ class SeleniumDriver():
             exam_element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//*[text()='Betala senare']"))
             )
-            exam_element.click()
-            exam_element_parent = exam_element.find_element_by_xpath('..')
-            exam_element_parent.click()
+            exam_element.find_element(By.XPATH, '..').click()
         except TimeoutException:
             print("Took too long to load")
         except NoSuchElementException:
@@ -166,9 +159,7 @@ class SeleniumDriver():
             exam_type_element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.XPATH, f"//button[@class='btn btn-primary btn-lg btn-block']"))
             )
-            exam_type_element.click()
-            exam_element_parent = exam_type_element.find_element_by_xpath('..')
-            exam_element_parent.click()
+            exam_type_element.find_element(By.XPATH, '..').click()
         except TimeoutException:
             print("Took too long to load")
         except NoSuchElementException:
@@ -199,6 +190,7 @@ def find_exam(driver):
                         driver.continue_running = driver.select_time(config.dates[j], config.dates[j+1])
                         time.sleep(0.1)
                         if not driver.continue_running:
+                            time.sleep(5)
                             driver.book_exam()
                             time.sleep(5)
                             driver.confirm_booking()
